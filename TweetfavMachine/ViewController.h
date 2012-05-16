9ae4f97ea26483fa0e52db55f306413713cf226e
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-05-16 18:14:55 +0900

    Initial Commit

---
+++ TweetfavMachine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TweetfavMachine
+//
+//  Created by 真有 津坂 on 12/05/16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
